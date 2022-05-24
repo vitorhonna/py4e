3162a2ed69fa11cd648256c232034a745ee1f84d
Vitor Honna <vitorhonna@gmail.com>
2022-05-24 09:32:00 -0300

    course 3

--- 2_Python_Data_Structures/frequency_counter.py
+++ 2_Python_Data_Structures/frequency_counter.py
@@ -1,5 +1,6 @@
 separator = "-"
 number_separtors = 50
+
 print(number_separtors*separator)
 
 fh = open("romeo.txt", "r")
@@ -8,6 +9,7 @@ for line in fh:
     print(line.strip())
     count += 1
 fh.close()
+
 print(count, "Lines")
 print(number_separtors*separator)
 
@@ -19,16 +21,16 @@ for line in fh:
     for word in words:
         wordsFreq[word] = wordsFreq.get(word, 0)+1
 fh.close()
-print(number_separtors*separator)
 
+print(number_separtors*separator)
 print(wordsFreq)
 print(number_separtors*separator)
 
 wordsFreq_sorted = list(wordsFreq.items())
 wordsFreq_sorted.sort(reverse=True, key=lambda x: x[1])
+
 print(wordsFreq_sorted)
 print(number_separtors*separator)
-
 print(
     f'The most frequent word is "{wordsFreq_sorted[0][0]}", which appears {wordsFreq_sorted[0][1]} times')
 print(number_separtors*separator)
